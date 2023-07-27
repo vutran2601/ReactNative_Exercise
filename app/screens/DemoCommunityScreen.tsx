5a2ad50ae4ca0105fd15b5991a836bc1d398bbc4
VuTran2601 <nguyenvutran2601@gmail.com>
2023-07-27 07:16:32 +0700

    Update DemoCommunityScreen.tsx

--- app/screens/DemoCommunityScreen.tsx
+++ app/screens/DemoCommunityScreen.tsx
@@ -2,16 +2,10 @@ import React, { FC } from "react"
 import { Screen, Text } from "../components"
 import { DemoTabScreenProps } from "../navigators/DemoNavigator"
 import { View, Image } from "react-native"
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon from "react-native-vector-icons/FontAwesome"
 
-
-
-export const DemoCommunityScreen: FC<DemoTabScreenProps<"Shopping">> =
-  function DemoCommunityScreen(_props) {
-    return (
-      <Screen preset="scroll" safeAreaEdges={["top"]}>
-        
-      </Screen>
-    )
-  }
-
+export const DemoCommunityScreen: FC<DemoTabScreenProps<"Shopping">> = function DemoCommunityScreen(
+  _props,
+) {
+  return <Screen preset="scroll" safeAreaEdges={["top"]}></Screen>
+}
